af87569285bd575f55e8ed891c74481db76e9a62
David <davidbazanticse@gmail.com>
2024-02-20 12:25:20 +0000

    Complete basic EMG control

--- sketch_oct14a.ino
+++ sketch_oct14a.ino
@@ -1,3 +1,5 @@
+#include <EMGFilters.h>
+
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
 #else
@@ -5,115 +7,67 @@
 #endif
 
 #include "EMGFilters.h"
+#include <Servo.h>
 
 #define TIMING_DEBUG 1
 
-#define SensorInputPin 25 // input pin number
-
-#define MAX(a,b) (((a)>(b))?(a):(b))
+#define SensorInputPin A0
 
 EMGFilters myFilter;
-// discrete filters must works with fixed sample frequence
-// our emg filter only support "SAMPLE_FREQ_500HZ" or "SAMPLE_FREQ_1000HZ"
-// other sampleRate inputs will bypass all the EMG_FILTER
-SAMPLE_FREQUENCY  sampleRate = SAMPLE_FREQ_1000HZ;
-// For countries where power transmission is at 50 Hz
-// For countries where power transmission is at 60 Hz, need to change to
-// "NOTCH_FREQ_60HZ"
-// our emg filter only support 50Hz and 60Hz input
-// other inputs will bypass all the EMG_FILTER
-NOTCH_FREQUENCY humFreq = NOTCH_FREQ_50HZ;
+int sampleRate = SAMPLE_FREQ_1000HZ;
+int humFreq = NOTCH_FREQ_50HZ;
 
-// Calibration:
-// put on the sensors, and release your muscles;
-// wait a few seconds, and select the max value as the threshold;
-// any value under threshold will be set to zero
-uint32_t Threshold = 900000;
+static int Threshold = 10;
 
 unsigned long timeStamp;
 unsigned long timeBudget;
 
+Servo myServo;
+const int numSamples = 150; // Adjust the number of samples for rolling average
+int window[numSamples];
+int sampleIndex = 0;
+int state = 0;
+
 void setup() {
-    /* add setup code here */
     myFilter.init(sampleRate, humFreq, true, true, true);
 
-    // open serial
     Serial.begin(115200);
 
-    // setup for time cost measure
-    // using micros()
     timeBudget = 1e6 / sampleRate;
-    // micros will overflow and auto return to zero every 70 minutes
-    //calibrateThreshold();
-}
 
-int readnumber(void) {
-  int num = 0;
-  boolean validnum = false;
-
-  while (1) {
-    while (!Serial.available());
-    char c = Serial.read();
-    if (isdigit(c)) {
-      num *= 10;
-      num += c - '0';
-      validnum = true;
-    } else if (validnum) {
-      return num;
+    myServo.attach(A1);
+    for (int i = 0; i < numSamples; ++i) {
+      window[i] = 0;
     }
-  }
 }
-
 
 void loop() {
-    /* add main program code here */
-    // In order to make sure the ADC sample frequence on arduino,
-    // the time cost should be measured each loop
-    /*------------start here-------------------*/
-
-    if (Serial.available()) {
-        Threshold = readnumber(); 
-        // Serial.println(Threshold);
-    }
-
-
-
     timeStamp = micros();
 
     int Value = analogRead(SensorInputPin);
-
-    // filter processing
     int DataAfterFilter = myFilter.update(Value);
 
-    int envlope = sq(DataAfterFilter);
-    // any value under threshold will be set to zero
-    Serial.print(envlope);
-    Serial.print(",");
-    envlope = (envlope > Threshold) ? envlope : 0;
+    int envlope = abs(DataAfterFilter);
 
     timeStamp = micros() - timeStamp;
-    if (TIMING_DEBUG) {
-        // Serial.print("Read Data: "); Serial.println(Value);
-        // Serial.print("Filtered Data: ");Serial.println(DataAfterFilter);
-        // Serial.print("Squared Data: ");
-        Serial.print(envlope);
-        Serial.print(",");
-        Serial.print(Threshold);
-        Serial.println();
-        // Serial.print(envlope);
-        // Serial.print(" ");
-        // Serial.print(sq(DataAfterFilter));
-        // Serial.print(" ");
-        // Serial.println(Threshold);
-        //Serial.println(Value);
-        // Serial.print("Filters cost time: "); Serial.println(timeStamp);
-        // the filter cost average around 520 us
+    // Serial.println(envlope);
+
+    // Update the rolling average
+    window[sampleIndex] = envlope;
+    sampleIndex = (sampleIndex + 1) % numSamples;
+    int sum = 0;
+    for (int i = 0; i < numSamples; ++i) {
+        sum += window[i];
+    }
+    int rollingAverage = sum / numSamples;
+
+    Serial.println(rollingAverage);
+
+    if (rollingAverage > Threshold) {
+      myServo.write(180);
+    } else {
+      myServo.write(0);
     }
 
-    /*------------end here---------------------*/
-    // if less than timeBudget, then you still have (timeBudget - timeStamp) to
-    // do your work
     delayMicroseconds(500);
-    // if more than timeBudget, the sample rate need to reduce to
-    // SAMPLE_FREQ_500HZ
 }
